1940d7f6603ab224e8160e78d8c671705449f6ca
Philipp <pd95@users.noreply.github.com>
2023-09-04 20:59:31 +0200

    Initial Commit

---
+++ MapToy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapToy
+//
+//  Created by Philipp on 04.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapToy/MapToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapToyApp.swift
+//  MapToy
+//
+//  Created by Philipp on 04.09.23.
+//
+
+import SwiftUI
+
+@main
+struct MapToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
